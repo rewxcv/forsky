1e44dd9ee68fcb8c51a2df394b08167c6b08fabf
Xu Shen <shensxsx@cn.ibm.com>
2021-06-05 08:25:36 +0800

    123

--- pinyinpractise/index.js
+++ pinyinpractise/index.js
@@ -56,6 +56,6 @@ function genPinyins(mustInclude, outFormat){
     })
   })
 }
-var mustInclude = "文次找平办让包"
+var mustInclude = "钟丁元共已经坐要连百还舌点块非常往瓜进空"
 genPinyins(mustInclude, OUTPUT_FOFRMAT.PINYIN_CHARS_JSON);
 //module.exports = genPinyins;
